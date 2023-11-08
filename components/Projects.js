1019fd2527aee012581ae2f660c39d504cc3532d
Alexandru Panta <alex@alexpanta.dev>
2023-11-08 15:17:07 +0100

    projects.sort

--- components/Projects.js
+++ components/Projects.js
@@ -11,6 +11,165 @@ function Projects(id) {
   return (
     <div className={styles.Projects} id='section2'>
       <h1 className={styles.Title}>Projects</h1>
+    <div className={styles.container_text_image}>
+      <div className={styles.container_project}>
+        <div className={styles.textLink}>
+              <video className={styles.video} controls>
+                <source src='/Ecna.mp4' />
+              </video>
+          <div className={styles.textInfo}>
+            <p className={styles.textStyle}>
+            ECNA is a mobile application related to the medical transportation sector, primarily intended for private ambulance companies. 
+            Its main purpose is to provide real-time internal tracking of activities, including past, ongoing, and future assignments. 
+            Additionally, it aims to offer significant time savings and a comfortable support experience for all platform users 🚑
+            </p>
+            <div className={styles.image_lang}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src="/node.png"
+                  width={'100%'}
+                  height={'100%'}
+                  alt="node.js"
+                  style={imageStyle}
+                />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image
+                  src="/ex.png"
+                  width={'100%'}
+                  height={'100%'}
+                  alt="express.js"
+                  style={imageStyle}
+                />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image
+                  src="/mongodb.png"
+                  width={'100%'}
+                  height={'100%'}
+                  alt="mongoDB"
+                  style={imageStyle}
+                />
+              </div>
+              <div className={styles.imageContainer_reactNative}>
+                <Image
+                  src="/react-native.png"
+                  width={'135%'}
+                  height={'150%'}
+                  alt="react-native"
+      
+                />
+              </div>
+            </div>
+          <div className={styles.link_container}>
+          <Link href='https://github.com/AlexandruPanta/Ecna.git'>
+            <span className={styles.link}>github
+              <Image
+                src="/github.png"
+                width={'20%'}
+                height={'20%'}
+                alt="github"
+                style={imageStyle}
+              />
+            </span>
+          </Link>
+        </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      <div className={styles.container_text_image}>
+        <div className={styles.container_project}>
+          <div className={styles.textLink}>
+            <Image
+              src="/morningnews.png"
+              width={500}
+              height={300}
+              alt="Picture of the author"
+              style={imageStyle}
+            />
+            <div className={styles.textInfo}>
+              <p className={styles.textStyle}>
+              Morning News redefines how you consume the news. With top-tier security (bcrypt and uid2) 
+              for registration and login, your data is safeguarded.
+              Customize your news feed effortlessly - bookmark articles you love, hide unwanted ones with 
+              the eye slash icon, and revisit them with a single click. Your bookmarked articles are neatly stored on your personal bookmarks page.
+              Morning News: Secure. Personalized. Informative  📰</p>
+              <div className={styles.image_lang}>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src="/node.png"
+                    width={'100%'}
+                    height={'100%'}
+                    alt="Node.js"
+                    style={imageStyle}
+                  />
+                </div>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src="/ex.png"
+                    width={'100%'}
+                    height={'100%'}
+                    alt="Express.js"
+                    style={imageStyle}
+                  />
+                </div>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src="/mongodb.png"
+                    width={'100%'}
+                    height={'100%'}
+                    alt="MongoDB"
+                    style={imageStyle}
+                  />
+                </div>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src="/react.png"
+                    width={'100%'}
+                    height={'100%'}
+                    alt="React"
+                    style={imageStyle}
+                  />
+                </div>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src="/next.png"
+                    width={'100%'}
+                    height={'100%'}
+                    alt="Next.js"
+                    style={imageStyle}
+                  />
+                </div>
+              </div>
+              <div className={styles.link_container}>
+              <Link href='https://morning-news-frontend-3ydpgcqdq-alexandrupanta.vercel.app/'>
+                <span className={styles.link}>Demo
+                <Image
+                    src="/internet.png"
+                    width={'20%'}
+                    height={'20%'}
+                    alt="internet"
+                    style={imageStyle}
+                  />
+                </span>
+              </Link>
+              <Link href='https://github.com/AlexandruPanta/MorningNews'>
+                <span className={styles.link}>github
+                <Image
+                    src="/github.png"
+                    width={'20%'}
+                    height={'20%'}
+                    alt="github"
+                    style={imageStyle}
+                  />
+                  </span>
+              </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className={styles.container_text_image}>
         <div className={styles.container_project}>
           <div className={styles.textLink}>
@@ -104,91 +263,64 @@ function Projects(id) {
       <div className={styles.container_text_image}>
         <div className={styles.container_project}>
           <div className={styles.textLink}>
-            <Image
-              src="/morningnews.png"
-              width={500}
-              height={300}
-              alt="Picture of the author"
-              style={imageStyle}
-            />
+                <video className={styles.video} controls>
+                  <source src='/Movie-directory.mp4' />
+                </video>
             <div className={styles.textInfo}>
               <p className={styles.textStyle}>
-              Morning News redefines how you consume the news. With top-tier security (bcrypt and uid2) 
-              for registration and login, your data is safeguarded.
-              Customize your news feed effortlessly - bookmark articles you love, hide unwanted ones with 
-              the eye slash icon, and revisit them with a single click. Your bookmarked articles are neatly stored on your personal bookmarks page.
-              Morning News: Secure. Personalized. Informative  📰</p>
+              Welcome to the Movie Directory project! This is a comprehensive directory that provides detailed information about a
+               wide range of movies. 
+              It's designed with a user-friendly interface and robust functionality to enhance your movie exploration experience 🍿
+              </p>
               <div className={styles.image_lang}>
+                  <Image
+                    src="/flask.png"
+                    width={'56%'}
+                    height={'56%'}
+                    alt="flask"
+                    style={imageStyle}
+                  />
                 <div className={styles.imageContainer}>
                   <Image
-                    src="/node.png"
+                    src="/mysql.png"
                     width={'100%'}
                     height={'100%'}
-                    alt="Node.js"
+                    alt="mysql"
                     style={imageStyle}
                   />
                 </div>
                 <div className={styles.imageContainer}>
                   <Image
-                    src="/ex.png"
+                    src="/html.png"
                     width={'100%'}
                     height={'100%'}
-                    alt="Express.js"
+                    alt="html"
                     style={imageStyle}
                   />
                 </div>
                 <div className={styles.imageContainer}>
                   <Image
-                    src="/mongodb.png"
+                    src="/css.png"
                     width={'100%'}
                     height={'100%'}
-                    alt="MongoDB"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/react.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="React"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/next.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="Next.js"
+                    alt="css"
                     style={imageStyle}
                   />
                 </div>
               </div>
-              <div className={styles.link_container}>
-              <Link href='https://morning-news-frontend-3ydpgcqdq-alexandrupanta.vercel.app/'>
-                <span className={styles.link}>Demo
+            <div className={styles.link_container}>
+            <Link href='https://github.com/AlexandruPanta/Movie-Directory'>
+              <span className={styles.link}>github
                 <Image
-                    src="/internet.png"
-                    width={'20%'}
-                    height={'20%'}
-                    alt="internet"
-                    style={imageStyle}
-                  />
-                </span>
-              </Link>
-              <Link href='https://github.com/AlexandruPanta/MorningNews'>
-                <span className={styles.link}>github
-                <Image
-                    src="/github.png"
-                    width={'20%'}
-                    height={'20%'}
-                    alt="github"
-                    style={imageStyle}
-                  />
-                  </span>
-              </Link>
-              </div>
+                  src="/github.png"
+                  width={'20%'}
+                  height={'20%'}
+                  alt="github"
+                  style={imageStyle}
+                />
+              </span>
+            </Link>
+          </div>
             </div>
           </div>
         </div>
@@ -263,138 +395,6 @@ function Projects(id) {
                   </span>
               </Link>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.container_text_image}>
-        <div className={styles.container_project}>
-          <div className={styles.textLink}>
-                <video className={styles.video} controls>
-                  <source src='/Movie-directory.mp4' />
-                </video>
-            <div className={styles.textInfo}>
-              <p className={styles.textStyle}>
-              Welcome to the Movie Directory project! This is a comprehensive directory that provides detailed information about a
-               wide range of movies. 
-              It's designed with a user-friendly interface and robust functionality to enhance your movie exploration experience 🍿
-              </p>
-              <div className={styles.image_lang}>
-                  <Image
-                    src="/flask.png"
-                    width={'56%'}
-                    height={'56%'}
-                    alt="flask"
-                    style={imageStyle}
-                  />
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/mysql.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="mysql"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/html.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="html"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/css.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="css"
-                    style={imageStyle}
-                  />
-                </div>
-              </div>
-            <div className={styles.link_container}>
-            <Link href='https://github.com/AlexandruPanta/Movie-Directory'>
-              <span className={styles.link}>github
-                <Image
-                  src="/github.png"
-                  width={'20%'}
-                  height={'20%'}
-                  alt="github"
-                  style={imageStyle}
-                />
-              </span>
-            </Link>
-          </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.container_text_image}>
-        <div className={styles.container_project}>
-          <div className={styles.textLink}>
-                <video className={styles.video} controls>
-                  <source src='/Ecna.mp4' />
-                </video>
-            <div className={styles.textInfo}>
-              <p className={styles.textStyle}>
-              ECNA is a mobile application related to the medical transportation sector, primarily intended for private ambulance companies. 
-              Its main purpose is to provide real-time internal tracking of activities, including past, ongoing, and future assignments. 
-              Additionally, it aims to offer significant time savings and a comfortable support experience for all platform users 🚑
-              </p>
-              <div className={styles.image_lang}>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/node.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="node.js"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/ex.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="express.js"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/mongodb.png"
-                    width={'100%'}
-                    height={'100%'}
-                    alt="mongoDB"
-                    style={imageStyle}
-                  />
-                </div>
-                <div className={styles.imageContainer_reactNative}>
-                  <Image
-                    src="/react-native.png"
-                    width={'135%'}
-                    height={'150%'}
-                    alt="react-native"
-        
-                  />
-                </div>
-              </div>
-            <div className={styles.link_container}>
-            <Link href='https://github.com/AlexandruPanta/Ecna.git'>
-              <span className={styles.link}>github
-                <Image
-                  src="/github.png"
-                  width={'20%'}
-                  height={'20%'}
-                  alt="github"
-                  style={imageStyle}
-                />
-              </span>
-            </Link>
-          </div>
             </div>
           </div>
         </div>
